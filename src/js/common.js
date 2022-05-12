d9b3d7209921287e93c0979f9e6af1e1383b0457
wmmeg <wmmegal@gmail.com>
2022-05-12 20:18:26 +0300

    modals

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,44 @@
 document.addEventListener('alpine:init', function () {
-    Alpine.data('dropdown', () => ({
-        open: false,
+    Alpine.data('slider', () => ({
+        slider: '',
+        input: '',
+        currency: ' ₸',
+        typing() {
+            const triggerEvents = true,
+                value = Number(this.input.value.replace(this.currency));
+            this.slider.rangeSlider.update({value}, triggerEvents);
+            this.formattedValue(value)
+        },
+        clear() {
+            this.input.value = '';
+        },
+        formattedValue(value) {
+            this.input.value = value + this.currency;
+        },
+        init() {
+            this.slider = this.$el.querySelector('input[type=range]');
+            this.input = this.$el.querySelector('input[type=text]');
 
-        toggle() {
-            this.open = ! this.open
+            const that = this;
+
+            rangeSlider.create(this.slider, {
+                onSlideEnd(position, value) {
+                    that.formattedValue(position)
+                }
+            });
         }
     }))
+
+    Alpine.store('nav', {
+        isOpen: false,
+        media: matchMedia('(max-width: 1199px)'),
+
+        show() {
+            this.isOpen = true;
+            console.log(1);
+        },
+        hide() {
+            this.isOpen = false;
+        }
+    })
 });
